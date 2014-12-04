9ca8f4c35d577b8c89b0f6e4272c2700971ac427
Clement Brocard <clementbrocard@eduroam-032168.grenet.fr>
2014-12-04 10:16:38 +0100

    LibComm

--- proxy/src/client.c
+++ proxy/src/client.c
@@ -1,12 +1,14 @@
 #define BUFFER 256
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <strings.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include "../lib/libcomm/communicate.h"
 
 void error(const char *error){
     perror(error);
